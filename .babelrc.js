a6b734e35145be2ad5d0466c92a364bd15cb470e
Ankush Dhiman <ankush.dhiman@wildstyle-network.com>
2021-11-20 23:09:00 +0100

    babel config and lint config

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      require('@babel/preset-react'),
+      {
+        runtime: 'automatic',
+      },
+    ],
+    require('@babel/preset-typescript'),
+    require('@babel/preset-env'),
+  ],
+  plugins: [
+    process.env.NODE_ENV === 'development' && require.resolve('react-refresh/babel'),
+  ].filter(Boolean),
+};

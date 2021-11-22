1506525b4d8b8fdba69b9c86c291faa558565586
Ankush Dhiman <ankush.dhiman@wildstyle-network.com>
2021-11-22 17:29:04 +0100

    types from swagger

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,27 @@ export interface User {
   username: string;
   email: string;
 }
+
+export interface Boards {
+  id: string;
+  name: string;
+}
+
+export type NewsStatus = 'draft' | 'published' | 'archives';
+
+export interface News {
+  id: string;
+  boardId: string;
+  author: string;
+  title: string;
+  description: string;
+  imageURL: string;
+  createdAt: string;
+  status: NewsStatus;
+}
+
+export interface BoradNews {
+  draft: News[];
+  published: News[];
+  archives: News[];
+}

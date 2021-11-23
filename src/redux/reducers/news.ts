8d8d849e49c7765c9fea999562747243fcc9d6dd
Ankush Dhiman <ankush.dhiman@wildstyle-network.com>
2021-11-23 23:03:48 +0100

    upadates to RTK queries cache invalidation using tags

--- src/redux/reducers/news.ts
+++ src/redux/reducers/news.ts
@@ -1,17 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { News } from '../../types';
+import { News, Board, BoardNews } from '../../types';
 
 export const newsApiSlice = createApi({
   reducerPath: 'newsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:8080/v1',
   }),
-  tagTypes: ['News'],
+  tagTypes: ['News', 'Board'],
   endpoints(build) {
     return {
+      fetchBoards: build.query<Board[], void>({
+        query: () => '/board',
+        providesTags: ['Board'],
+      }),
+      fetchBoardNews: build.query<BoardNews, string>({
+        query: boardId => `/board/${boardId}/news`,
+        providesTags: ['News'],
+      }),
       fetchNews: build.query<News, string>({
         query: newsId => `/news/${newsId}`,
+        providesTags: ['News'],
       }),
       addNews: build.mutation<News, News>({
         query: news => ({
@@ -19,6 +28,7 @@ export const newsApiSlice = createApi({
           method: 'POST',
           body: news,
         }),
+        invalidatesTags: ['News'],
       }),
       updateNews: build.mutation<News, News>({
         query: news => ({
@@ -26,18 +36,21 @@ export const newsApiSlice = createApi({
           method: 'PUT',
           body: news,
         }),
+        invalidatesTags: ['News'],
       }),
       deleteNews: build.mutation<string, string>({
         query: newsId => ({
           url: `/news${newsId}`,
           method: 'DELETE',
         }),
+        invalidatesTags: ['News'],
       }),
       updateNewsStatus: build.mutation<string, Partial<News>>({
         query: ({ id, status }) => ({
           url: `/news${id}/${status}`,
           method: 'DELETE',
         }),
+        invalidatesTags: ['News'],
       }),
     };
   },
@@ -49,4 +62,6 @@ export const {
   useUpdateNewsMutation,
   useDeleteNewsMutation,
   useUpdateNewsStatusMutation,
+  useFetchBoardsQuery,
+  useFetchBoardNewsQuery,
 } = newsApiSlice;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user';
 import { newsApiSlice } from './reducers/news';
-import { boardApiSlice } from './reducers/board';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     [newsApiSlice.reducerPath]: newsApiSlice.reducer,
-    [boardApiSlice.reducerPath]: boardApiSlice.reducer,
   },
   middleware:
     getDefaultMiddleware => getDefaultMiddleware()
-      .concat(newsApiSlice.middleware)
-      .concat(boardApiSlice.middleware),
+      .concat(newsApiSlice.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;

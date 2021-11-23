4812c4b1f58a198ec33843ac79297957fe1cbf6e
Ankush Dhiman <ankush.dhiman@wildstyle-network.com>
2021-11-23 16:29:47 +0100

    UI updates for news card

--- src/components/cards/news.tsx
+++ src/components/cards/news.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { News } from '../../types';
 
 interface NewsCardProps {
@@ -5,14 +6,17 @@ interface NewsCardProps {
 }
 
 export default function NewsCard({
-  author, title, description, imageURL, className,
+  author, title, description, imageURL, className, id,
 }: NewsCardProps & News) {
   return (
-    <div className={`${className || ''} p-2 flex flex-col w-[300px] shadow-md`}>
+    <div className={`${className || ''} flex flex-col w-[300px] shadow-md`}>
       <img className='w-full mb-1.5' src={imageURL} alt={title}/>
-      <h3 className='mb-1.5'>{title}</h3>
-      <p className='mb-1.5'>{description}</p>
-      <small>{author}</small>
+      <div className='p-2 text-center'>
+        <h3 className='mb-1.5 text-bold text-2xl text-brand text-center'>{title}</h3>
+        <p className='mb-1.5 text-center'>{description}</p>
+        <small className='text-copy-light text-sm block text-center mb-2'>{author}</small>
+        <Link className='text-center text-brand hover:text-brand-light' to={`/news/${id}`}>EDIT</Link>
+      </div>
     </div>
   );
 }
